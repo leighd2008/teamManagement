5fed96239f1545a2f5be03bb4bbbb97d7ceb8283
Diane Leigh <leighd2008@gmail.com>
2023-08-16 12:19:02 -0400

    converted tryouts page from fire to generic

--- src/pages/tryoutspage/tryout.jsx
+++ src/pages/tryoutspage/tryout.jsx
@@ -8,9 +8,9 @@ const TryOuts = () => {
       <Card style={{ backgroundColor: "#6c757d00", borderColor: "#6c757d00", width: '100vw' }}>
         <CardBody>
           <CardTitle className="tc f1" tag="h1">
-            Are you ready to play with FIRE?
+            Are you ready to play?
           </CardTitle>
-          <h3>FIRE FASTPITCH team tryouts for all age groups will be </h3>
+          <h3>Your Organization team tryouts for all age groups will be </h3>
           <h1>July 30st, July 31st, Aug. 6th & Aug. 7th</h1>
           <h3>REGISTRATION:</h3>
           <a href="Preregistration" rel="noopener noreferrer">
@@ -18,30 +18,30 @@ const TryOuts = () => {
           </a>
           <h3> or at Field #1 on the day of tryouts. </h3>
           <br />
-          <h2>10U(2012 & 2013) 12U(2010 & 2011) 14U(2008 & 2009) </h2><h3>10:00AM-12:00PM  - ARIVE @ 9:30AM</h3>
+          <h2>Age Group 1 </h2><h3>10:00AM-12:00PM  - ARRIVE @ 9:30AM</h3>
           <br />
-          <h2>16U(2006 & 2007) 18U(2004 & 2005) </h2><h3>1:00-3:00PM  - ARIVE @ 12:30PM</h3>
+          <h2>Age Group 2 </h2><h3>1:00-3:00PM  - ARRIVE @ 12:30PM</h3>
           <br />
-          <h2>*PITCHERS & CATCHERS</h2> <h3> will need to stay an additional 30 min to be evaluated separately</h3>
+          <h2>*Special group</h2> <h3> special instructions</h3>
           <br />
-          <h2>Please be sure that the girls are dressed & equipped as they would for a game. Arrive hydrated and bring water.</h2>
+          <h2>Please be sure that players are dressed & equipped as they would for a game. Arrive hydrated and bring water.</h2>
           <br />
-          <h3>There is nothing to be nervous about, we look for much more than home runs & 70 MPH fastballs. We look for character, confidence and hustle!!!</h3>
+          <h3>Anything else you may want to say</h3>
           <br />
-          <h2>LOCATION:</h2>
-          <a
+          <h2>LOCATION: map link</h2>
+          {/* <a
             href={`https://goo.gl/maps/TSZ2T6Ti4oo`}
             rel="noopener noreferrer"
             target="_blank"
-          >
-            <h3>800 Gillmer Rd <br /> Leavittsbug Ohio 44430</h3>
-          </a>
+          > */}
+            <h3>address</h3>
+          {/* </a> */}
           <br />
 
-          <h2>If you or anyone you know is interested in discussing a coaching opportunity or would like to get a private tryout for your daughter please contact us: </h2>
-          <h3>Fire.Fastpitch.Softball@gmail.com</h3>
+          <h2>Something else to add? </h2>
+          <h3>yourOrganization@email.com</h3>
           <br />
-          <h3>or call 330-980-1366</h3>
+          <h3>organization phone number</h3>
           <br />
         </CardBody>
       </Card>
